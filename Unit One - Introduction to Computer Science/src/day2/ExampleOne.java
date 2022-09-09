7f933a0e4dc4f3b87f85232c49b0b2e7d5ad5857
Dilan Jamil <djamil@bayviewglen.ca>
2022-09-09 11:09:56 -0400

    testing git

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,24 @@
+package day2;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to computer science");
+        System.out.println("welcome to computer science");
+        System.out.println("welcome to computer science");
+        System.out.print('A');
+        
+        System.out.print('B');
+        System.out.print('c');
+        System.out.print('d');
+        System.out.print("e");
+        System.out.print('f');
+    
+    }
+    
+}
+
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,17 @@
+package day2;
+/*
+ * This example is about escape sequences 
+ */
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("computer science is the \"BEST\"class ever!");
+        System.out.println("\\");
+
+         // 
+        
+         
+    
+    
+    }
+    
+}
